750ead842ff05f50d991110e4d7d075fac30d1a5
swapnil7.more <swapnil7.more@ril.com>
2025-07-02 16:19:03 +0530

    added bts section

--- src/pages/bts/bts.tsx
+++ src/pages/bts/bts.tsx
@@ -1,12 +1,25 @@
 import './bts.scss'
 
 const BehindTheScene = () => {
+
+    const btsImages = [
+        'bts1.jpg',
+        'bts2.jpg',
+        'bts4.jpg',
+        'bts5.jpg',
+        'bts3.jpg',
+    ]
     return (
         <div className='bts-wrapper'>
             <div className='title'>Behind The Scene</div>
-            <div>
-                
+            <div className='bts-image-container'>
+                {btsImages.map((image, index) => (
+                    <div key={index}>
+                        <img className='bts-image' src={`/assets/icons/${image}`} alt="behind the scene" />
+                    </div>
+                ))}
             </div>
+
         </div>
     )
 }
