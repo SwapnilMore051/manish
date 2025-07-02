ca17b47c7d7b32089d1e04087f1e1078d8762da2
SwapnilMore051 <102518272+SwapnilMore051@users.noreply.github.com>
2025-07-02 20:07:45 +0530

    Update bts.tsx added bts-video-container

--- src/pages/bts/bts.tsx
+++ src/pages/bts/bts.tsx
@@ -1,27 +1,51 @@
-import './bts.scss'
+import "./bts.scss";
 
 const BehindTheScene = () => {
+  const btsImages = [
+    "bts1.jpg",
+    "bts2.jpg",
+    "bts4.jpg",
+    "bts5.jpg",
+    "bts3.jpg",
+  ];
 
-    const btsImages = [
-        'bts1.jpg',
-        'bts2.jpg',
-        'bts4.jpg',
-        'bts5.jpg',
-        'bts3.jpg',
-    ]
-    return (
-        <div className='bts-wrapper'>
-            <div className='title'>Behind The Scene</div>
-            <div className='bts-image-container'>
-                {btsImages.map((image, index) => (
-                    <div key={index}>
-                        <img className='bts-image' src={`/assets/icons/${image}`} alt="behind the scene" />
-                    </div>
-                ))}
-            </div>
+  const btsVideos = [
+    "https://www.youtube.com/embed/Ls02w5BTWFY",
+    "https://www.youtube.com/embed/Ls02w5BTWFY",
+  ];
 
+  return (
+    <div className="bts-wrapper">
+      <div className="title">Behind The Scene</div>
+      <div className="bts-image-container">
+        {btsImages.map((image, index) => (
+          <div key={index}>
+            <img
+              className="bts-image"
+              src={`/assets/icons/${image}`}
+              alt="behind the scene"
+            />
+          </div>
+        ))}
+          
         </div>
-    )
-}
+        <div className= "bts-video-container">
 
-export default BehindTheScene
+        {btsVideos.map((video, index) => (
+          <div key={index}>
+            <iframe
+              className="bts-video"
+              src={video}
+              title={`YouTube video ${index + 1}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BehindTheScene;
