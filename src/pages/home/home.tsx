ed13d1cce338709ab1d28dd441e697d0a209c770
SwapnilMore051 <102518272+SwapnilMore051@users.noreply.github.com>
2025-07-06 19:59:26 +0530

    Update home.tsx just welcome

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,7 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <div className="main-heading">Welcome to Foley</div>
+      <div className="main-heading">Welcome</div>
       <div className="sub-heading">
         <div className="sub-titles">I bring visuals to life -</div>
         <div className="sub-titles">"Foley is my art"</div>
