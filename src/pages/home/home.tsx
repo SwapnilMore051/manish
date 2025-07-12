fcabc01352b5cf0b0cc3f95e715891f737731909
swapnil7.more <swapnil7.more@ril.com>
2025-07-12 16:21:03 +0530

    added secret admin tab in welcome

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import "./home.scss";
+
 const Home = () => {
+
+  const navigate = useNavigate();
+  const adminNavigate = () => {
+    navigate("/admin");
+
+  };
   return (
     <div className="home-wrapper">
-      <div className="main-heading">Welcome</div>
+      <div className="main-heading" onClick={adminNavigate}>Welcome</div>
       <div className="sub-heading">
         <div className="sub-titles">I bring visuals to life -</div>
         <div className="sub-titles">"Foley is my art"</div>
