cfedd6eb900790b829d7452ce5ef8e9cc31fba2c
SwapnilMore051 <102518272+SwapnilMore051@users.noreply.github.com>
2025-09-20 13:44:15 +0530

    Update login.tsx added email password routes

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import EmailInput from "../components/Email-input";
-import PasswordInput from "../components/Password-input";
+import EmailInput from "../../components/inputs/email-input";
+import PasswordInput from "../../components/inputs/password-input";
 import type { InputState } from "../models/input";
 
 const Login = () => {
