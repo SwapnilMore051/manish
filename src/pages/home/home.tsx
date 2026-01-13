a44d57d33b422163763f53d13cf0c32dd25bd345
swapnil7.more <swapnil7.more@ril.com>
2026-01-13 13:03:19 +0530

    removed video as a background

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,28 +6,14 @@ const Home = () => {
   const loginNavigate = () => {
     navigate("/login");
   };
-
   return (
     <div className="home-wrapper">
-      {/* Background video */}
-      <video
-        className="background-video"
-        src="https://res.cloudinary.com/didtglcy3/video/upload/v1767166213/BGvideo_cwaf55.mp4"
-        autoPlay
-        muted
-        loop
-        playsInline
-      />
-
-      {/* Content on top of video */}
-      <div className="home-content">
-        <div className="main-heading" onClick={loginNavigate}>
-          Welcome
-        </div>
-        <div className="sub-heading">
-          <div className="sub-titles">I bring visuals to life -</div>
-          <div className="sub-titles">"Foley is my art"</div>
-        </div>
+      <div className="main-heading" onClick={loginNavigate}>
+        Welcome
+      </div>
+      <div className="sub-heading">
+        <div className="sub-titles">I bring visuals to life -</div>
+        <div className="sub-titles">"Foley is my art"</div>
       </div>
     </div>
   );
